fa9694bbc82a4218d75d1f3844735d117469e4c4
manulaz81 <78950476+manulaz81@users.noreply.github.com>
2022-01-11 11:08:05 +0100

    creation database

--- backend/config/bdd.sql
+++ backend/config/bdd.sql
@@ -1,16 +1,27 @@
-const mysql = require ('mysql');
+create database groupomo ;
 
-const db = mysql.createConnection({
-    host : "root@localhost",
-    user : "manulaz81",
-    password :  "manuel1981"
+create database if not exists groupomo;
 
-});
+use groupomo;
 
-db.connect(function(err){
-    if(err) throw err;
-    console.log("connecté à la base de données mySql!");
-    
-});  
 
-console.log(db);
+-- //  const mysql = require ('mysql');
+-- // const { Sequelize } = require('sequelize');
+
+-- // const sequelize = new Sequelize("foodly", "manulaz81", "manuel1981", {
+-- //     dialect: "mysql",
+-- //     host: "root@localhost"
+-- // });
+
+-- // try {
+-- //    sequelize.authenticate();
+-- //    console.log('Connecté à la base de données MySQL!');
+-- //  } catch (error) {
+-- //    console.error('Impossible de se connecter, erreur suivante :', error);
+-- //  };
+
+
+
+
+
+-- // 
