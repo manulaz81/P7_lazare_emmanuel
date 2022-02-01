b694c942b106d923363b1dd5089d45566fc57a27
manulaz81 <78950476+manulaz81@users.noreply.github.com>
2022-02-01 18:48:10 +0100

    page de connextion

--- front/src/components/signUp.vue
+++ front/src/components/signUp.vue
@@ -1,5 +1,12 @@
+<script>
+export default{
+name : 'navigation'
+}
+</script>
+
+
 <template>
 			<a href="/">Accueil</a>
 			<a href="/about">À propos</a>
 			<a href="/contact">Contact</a>
-		</template>
+</template>
